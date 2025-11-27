dc7df8fbc8dcc2571aba05981b215b1c60c39952
igirishbind-lab <igirishbind@gmail.com>
2025-11-27 20:09:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-export PYTHONPATH=.
-python advanced_bot_full.py
+#!/bin/bash
+pip install --upgrade pip
+pip install -r requirements.txt
+gunicorn --bind 0.0.0.0:$PORT advanced_bot_full:app
